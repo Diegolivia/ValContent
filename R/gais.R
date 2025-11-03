2e72e7a697e5e9343c69b44f1591c8f30b9db9f2
Cesar Merino-Soto <sikayax@yahoo.com.ar>
2025-11-03 15:08:08 -0600

    Create gais.R

---
+++ R/gais.R
@@ -0,0 +1,31 @@
+#' Example data: substantive validity of Confidence in the Future dimenison (Global Academic Interest Scale; GAIS).
+#'
+#' Partial data from the application of the GAIS to Peruvian undergraduate students, for the analysis of substantive validity;
+#' this data only evaluates the Confidence in the Future dimension (4 items).
+#' The numerical content (values from 1 to 4) identifies each of the four dimensions of the GAIS.:
+#' 1: Passion for learning
+#' 2: Confidence in the future
+#' 3: Career aspiration
+#' 4: Self-expression
+#'
+#' @format
+#' Un data frame con 30 filas y 4 columnas:
+#'
+#' \describe{
+#'   \item{gais1}{Item 1}
+#'   \item{gais2}{Item 2}
+#'   \item{gais2}{Item 3}
+#'   \item{gais2}{Item 4}
+#' }
+#'
+#' @source
+#' Analysis in progress to validate the content of the Global Academic Interest Scale (Lee & Durksen, 2020)
+#'
+#' @references Citas o referencias adicionales si aplica.
+#' Lee, J., & Durksen, T. L. (2020). Global Academic Interest Scale for Undergraduate and Graduate Students. Journal of Psychoeducational Assessment, 39(2), 182-196. https://doi.org/10.1177/0734282920946144
+#'
+#' @examples
+#' data(gais)
+#' head(gais)
+
+
